56813a285cb2e6e2e98b3a5ae69ef2de91562819
Admin <sabafathi1802@gmail.com>
2023-12-30 13:52:13 +0330

    Initial commit

---
+++ consultant/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class Consultant(AbstractUser):
+    # username
+    # password
+    first_name = models.CharField(max_length=150, blank=False)
+    last_name = models.CharField(max_length=150, blank=False)
+    phone_number = models.CharField(max_length=11, blank=False)
+    address = models.CharField(max_length=250, blank=False)
+    natural_number = models.CharField(max_length=20, blank=False)
+    # customer
+    # salary data
---
+++ insurance/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+
+class Insurance(models):
+    pass
