172c185869d4073afdafca49ba2c89f7d8555fa4
Admin <sabafathi1802@gmail.com>
2024-01-04 14:47:46 +0330

    consultant model updated

--- consultant/models.py
+++ consultant/models.py
@@ -10,5 +10,6 @@ class Consultant(AbstractUser):
     phone_number = models.CharField(max_length=11, blank=False)
     address = models.CharField(max_length=250, blank=False)
     natural_number = models.CharField(max_length=20, blank=False)
+    salary = models.FloatField(max_length=250, blank=False)
     # customer
     # salary data
