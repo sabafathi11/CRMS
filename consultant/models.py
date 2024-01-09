b3c93d81f7a60d39db78899a8cea9a9e5a8bf74f
Admin <sabafathi1802@gmail.com>
2024-01-09 20:33:35 +0330

    update models

--- consultant/models.py
+++ consultant/models.py
@@ -1,10 +1,9 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class Consultant(AbstractUser):
-    # username inherits from AbstractUser
-    # password inherits from AbstractUser
+class Consultant(models.Model):
+    username = models.CharField(max_length=150, blank=False)
+    password = models.CharField(max_length=150, blank=False)
     first_name = models.CharField(max_length=150, blank=False)
     last_name = models.CharField(max_length=150, blank=False)
     phone_number = models.CharField(max_length=11, blank=False)
