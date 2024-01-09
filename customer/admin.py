32f90db96ef13e06a7d68e81449dd53aed359710
shaghayeghamini <shaghayeghamini8181@gmail.com>
2024-01-09 21:57:47 +0330

    create adminstrator for customer model

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Customer
 
-# Register your models here.
+
+@admin.register(Customer)
+class Customer_admin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'phone_number', 'address', 'natural_number')
+    search_fields = ('username', 'first_name', 'last_name', 'phone_number')
+    ordering = ('username',)
