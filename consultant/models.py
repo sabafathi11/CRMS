883b6815558cc09bccdbbbe1e39b0dfd1dff3c7a
Admin <sabafathi1802@gmail.com>
2024-01-13 23:41:50 +0330

    update consultant model

--- consultant/models.py
+++ consultant/models.py
@@ -1,14 +1,8 @@
 from django.db import models
+from customer.models import Customer
 
 
-class Consultant(models.Model):
-    username = models.CharField(max_length=150, blank=False)
-    password = models.CharField(max_length=150, blank=False)
-    first_name = models.CharField(max_length=150, blank=False)
-    last_name = models.CharField(max_length=150, blank=False)
-    phone_number = models.CharField(max_length=11, blank=False)
-    address = models.CharField(max_length=250, blank=False)
-    natural_number = models.CharField(max_length=20, blank=False)
+class Consultant(Customer):
     salary = models.FloatField(max_length=250, blank=False)
-    # customer
-    # salary data
+
+
--- customer/models.py
+++ customer/models.py
@@ -10,4 +10,5 @@ class Customer(AbstractUser):
     phone_number = models.CharField(max_length=11, blank=False)
     address = models.CharField(max_length=250, blank=False)
     natural_number = models.CharField(max_length=20, blank=False)
+    kind = models.CharField(max_length=10, choices=(('1', 'customer'),('2', 'consultant')), default='1')
 
