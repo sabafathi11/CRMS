6976228f95f8460a0f7f580a9d21918d858c1b5d
shaghayeghamini <shaghayeghamini8181@gmail.com>
2024-01-14 11:22:10 +0330

    create view and url for consultant model page

--- consultant/urls.py
+++ consultant/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path
+from .views import my_page_cons
 
-urlpatterns = []
+urlpatterns = [
+    path('my_page_cons/<int:pk>/',my_page_cons,name='my_page_cons'),
+]
--- consultant/views.py
+++ consultant/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Consultant
 
-# Create your views here.
+def my_page_cons(request, pk):
+    user = get_object_or_404(Consultant, pk=pk)
+    return render(request, 'My_page_cons.html', {'user': user})
